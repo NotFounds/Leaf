eee6e3f1944fbd9e547c60bfbc488d10219e53ff
NotFounds <m134_warrock_rpg7@yahoo.com>
2018-07-01 15:33:51 +0900

    Change directory configuration
    - Separate app into master/player

---
+++ player/src/reducer.jsx
@@ -0,0 +1,15 @@
+const initialState = {
+};
+
+export default function reducer(state = initialState, action) {
+  switch(action.type) {
+    default:
+      return state;
+  }
+}
+
+function uuidv4() {
+  return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+    (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+  );
+}
